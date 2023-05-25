9dfb5d4b4a5d5669f4b5ac82fa785d6356090bc7
boakaidkamara <boakaidkamara@gmail.com>
2023-05-25 23:38:52 +0000

    Validate form fields and logging errors to the console

--- controller/authorController.js
+++ controller/authorController.js
@@ -1,3 +1,7 @@
+// include validater library 
+const { body, validationResult } = require("express-validator");
+
+
 // Display list of all author 
 exports.author_list = function(req,res){
     res.render('authors', {title:"Local Library | Authors"});
@@ -10,19 +14,82 @@ exports.author_details = function(req,res){
  
 // Display form to create new Author on GET.
 exports.author_create_get = function(req, res) {
-//     res.send(`<form method="POST" action="">
-//   <input type="text" name="username" placeholder="username">
-//   <input type="submit">
-// </form>`);
+
     res.render('add-author', {title:"Add Author"});
  
 };
 
 // Handle Author create on POST.
-exports.author_create_post = function(req, res) {
-    console.log("REQ BODY", req.body);
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+// exports.author_create_post = function(req, res) {
+
+//     let new_author = req.body;
+
+//     // validate new author 
+//     body("first-name")
+//         .trim()
+//         .isLength({ min: 1 })
+//         .escape()
+//         .withMessage("First name must be specified.")
+//         .isAlphanumeric()
+//         .withMessage("First name has non-alphanumeric characters.")
+
+    
+//     // Process request after validation and sanitization.
+//     const error = validationResult(req);
+//     if(error.isEmpty()){
+
+//         res.send({"new author":new_author}); 
+
+//     }
+//     else{
+
+//         console.log(error.array());
+
+//     }
+
+    
+
+// };
+
+exports.author_create_post = [
+
+    // validate and sanitize 
+    body("first-name")
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("First name must be specified.")
+        .isAlphanumeric()
+        .withMessage("First name has non-alphanumeric characters."),
+
+    // Process request after validation and sanitization.
+    (async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create Author object with escaped and trimmed data
+        
+
+        if (!errors.isEmpty()) {
+        // There are errors. Render form again with sanitized values/errors messages.
+        let user_error = errors.array();
+        console.log(user_error);
+        return;
+        } else {
+        // Data from form is valid.
+
+        // Save author.
+        await author.save();
+        // Redirect to new author record.
+        res.redirect(author.url);
+        }
+    }),
+
+    
+    
+
+];
+
 
 // Display Author delete form on GET.
 exports.author_delete_get = function(req, res) {
--- routes/catalog.js
+++ routes/catalog.js
@@ -53,7 +53,7 @@ router.get('/books', book_controller.book_list);
 router.get('/author/create', author_controller.author_create_get);
 
 // POST request for creating Author.
-router.post('/author/create', author_controller.author_create_post);
+router.post('/author/create', author_controller.author_create_post); 
 
 // GET request to delete Author.
 router.get('/author/:id/delete', author_controller.author_delete_get);
